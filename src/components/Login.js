8d1bf8eef41b71a664182f666ab6344a2587327a
MoabiSethojane <moabisethojane@gmail.com>
2022-09-30 05:00:30 -0700

    moabi update

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,7 +2,10 @@ import React from 'react'
 
 function Login() {
   return (
-    <div>Login</div>
+    <div>Login
+      <div><input type="email" placeholder="Enter Email"></input></div>
+    </div>
+    
   )
 }
 
