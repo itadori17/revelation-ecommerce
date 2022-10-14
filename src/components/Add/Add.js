55fbc94d53c0c9e02c45a40c35d53e119020efac
Katlego Makofane <katlegomakofane3@gmail.com>
2022-10-14 15:58:30 +0200

    updatedaddfun

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,10 +1,11 @@
-import React, { useRef } from "react";
+import React, { useRef ,useState} from "react";
 import { db } from ".././config/Firebase";
 import { doc, setDoc, addDoc } from "firebase/firestore";
 import { useCollectionData } from "react-firebase-hooks/firestore";
 import { collection } from "@firebase/firestore";
 import CmsCenter from "./CmsCenter";
 import { contains } from "@firebase/util";
+import './Add.css'
 
 function Add({ path }) {
   const query = collection(db, `products`);
@@ -17,7 +18,7 @@ function Add({ path }) {
   const prodSizes = useRef();
   const prodPrice = useRef();
   const prodQty = useRef();
-
+  const [show,setShow]=useState(false);
   const [docs, loading] = useCollectionData(query);
   console.log(docs);
 
@@ -78,85 +79,115 @@ function Add({ path }) {
     (<CmsCenter />),
     (
       <div className="rightSideProductsInfo">
+      
         {loading && "Loading..."}
         <form className="formProduct" onSubmit={handleSubmit}>
-          <div className="Addprod">
-            <div className="prod">
-              <h2>Add Product</h2>
-              <select ref={prodType}>
-                <option value="1">Select Type</option>
-                <option value="T-shirt">T-shirt</option>
-                <option value="Shorts">Shorts</option>
-                <option value="Shirt">Shirt</option>
-                <option value="Denim">Denim</option>
-              </select>
-            </div>
-            <div>
-              <select name="" id="" ref={brandCategory}>
-                <option value="Category">Select Category</option>
-                <option value="Category 2">Summer</option>
-                <option value="Category 3">Winter</option>
-                <option value="Category 4">Accessories</option>
-                <option value="Category 5">Sale</option>
-              </select>
-            </div>
+           <div className="Addprod">
+              <div className="prod">
+                 <h2>Add Product</h2>
+                   <select ref={prodType}>
+                      <option value="1">Select Type</option>
+                      <option value="T-shirt">T-shirt</option>
+                      <option value="Shorts">Shorts</option>
+                      <option value="Shirt">Shirt</option>
+                      <option value="Denim">Denim</option>
+                   </select>
+                </div>
+              <div>
+                   <select name="" id="" ref={brandCategory}>
+                      <option value="Category">Select Category</option>
+                      <option value="Category 2">Summer</option>
+                      <option value="Category 3">Winter</option>
+                      <option value="Category 4">Accessories</option>
+                      <option value="Category 5">Sale</option>
+                   </select>
+              </div>
+
+              <div>
+                   <input type="text" placeholder="Product Name" ref={prodName} />
+              </div>
 
             <div>
-              <input type="text" placeholder="Product Name" ref={prodName} />
+                  <textarea
+                  type="text"
+                  placeholder="About the product"
+                  ref={prodDescription}
+                />
+            </div>
+            <div>
+                <input type="alphanumeric" placeholder="Code"></input>
+            </div>
+               <div>{/* <input type='file' ref={prodImage}/> */}</div>
+            <div>
+              <button type="submit" onClick={()=>setShow (!show )}>ADD PRODUCT</button>
             </div>
 
-            <div>
-              <textarea
-                type="text"
-                placeholder="About the product"
-                ref={prodDescription}
-              />
-            </div>
-            <div>{/* <input type='file' ref={prodImage}/> */}</div>
-            <div>
-              <button type="submit">ADD PRODUCT</button>
-            </div>
+
+            
+
           </div>
+          { show && 
+          <div className='Addsizes' >
+                <h2>Product Features</h2>
+                <div className='sizes'>
+                    
+                <label>Available colors</label>
+                <div>
+                   <select ref={prodColor}>
+                      <option value="default">Select Type</option>
+                      <option value="black">black</option>
+                      <option value="red">red</option>
+                      <option value="blue">blue</option>
+                      <option value="white">white</option>
 
-          <input type="alphanumeric" placeholder="Code"></input>
+                      <option value="XL">XL</option>
+                      <option value="XXL">XXL</option>
+                      <option value="XXXL">XXXL</option>
+                   </select>
+                </div>
+              <div>
+                  <label>Available sizes</label> 
+                  <div>
+                    <select ref={prodSizes}>
+                      <option value="XS">Select Type</option>
+                      <option value="XS">XS</option>
+                      <option value="S">S</option>
+                      <option value="M">M</option>
+                      <option value="L">L</option>
+
+                      <option value="XL">XL</option>
+                      <option value="XXL">XXL</option>
+                      <option value="XXXL">XXXL</option>
+                   </select>
+                 </div>
+              </div>
+              <div>
+               <label>Price</label> 
+                <div>
+                  <input
+                   type="number"
+                   step="0.01"
+                    placeholder="Price"
+                   ref={prodPrice}
+                  />
+                </div>
+              </div>
+             <div>
+              <label> Quantity</label> 
+                <div>
+                  <input type="number" placeholder="Quantity" ref={prodQty}/>
+                </div>
+                
+             </div>
+                <button type="submit">Add</button>
+             </div>
+            </div>
+}
         </form>
-        <form className="formProduct" onSubmit={colorSubmit}>
-          <p>Available colors</p>
-          <select ref={prodColor}>
-            <option value="default">Select Type</option>
-            <option value="black">black</option>
-            <option value="red">red</option>
-            <option value="blue">blue</option>
-            <option value="white">white</option>
-
-            <option value="XL">XL</option>
-            <option value="XXL">XXL</option>
-            <option value="XXXL">XXXL</option>
-          </select>
-          <div className="sizes">
-            <p>Available sizes</p>
-            <select ref={prodSizes}>
-              <option value="XS">Select Type</option>
-              <option value="XS">XS</option>
-              <option value="S">S</option>
-              <option value="M">M</option>
-              <option value="L">L</option>
-
-              <option value="XL">XL</option>
-              <option value="XXL">XXL</option>
-              <option value="XXXL">XXXL</option>
-            </select>
-          </div>
-          <input
-            type="number"
-            step="0.01"
-            placeholder="Price"
-            ref={prodPrice}
-          ></input>
-          <input type="number" placeholder="Quantity" ref={prodQty}></input>
-          <button type="submit">Add</button>
+        {/* <form className="formProduct" onSubmit={colorSubmit}>
+          
         </form>
-       
+         */}
       </div>
     )
   );
