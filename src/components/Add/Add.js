4c823f923d388cde72b914e75a6e334868b29cb7
KBModib <kmodibedi@outlook.com>
2022-10-14 12:54:25 +0200

    add.js works

--- src/components/Add/Add.js
+++ src/components/Add/Add.js
@@ -1,311 +1,163 @@
-import { getDatabase, ref as ref_database, child, get,push } from "firebase/database";
-
-import React, {useEffect, useState} from 'react';
-import { uid } from "uid";
-import { db } from "../config/Firebase";
-import {
-  collection,
-  getDocs,
-  addDoc,
-  updateDoc,
-  deleteDoc,
-  setDoc,
-  doc,
-  Timestamp
-} from "firebase/firestore";
-
-import './Add.css'
+import React, { useRef } from "react";
+import { db } from ".././config/Firebase";
+import { doc, setDoc, addDoc } from "firebase/firestore";
+import { useCollectionData } from "react-firebase-hooks/firestore";
+import { collection } from "@firebase/firestore";
 import CmsCenter from "./CmsCenter";
 
-function Add() {
+function Add({ path }) {
+  const query = collection(db, `products`);
+  const prodType = useRef();
+  const brandCategory = useRef();
+  const prodName = useRef();
+  const prodDescription = useRef();
+  // const prodImage = useRef();
+  const prodColor = useRef();
+  const prodSizes = useRef();
+  const prodPrice = useRef();
+  const prodQty = useRef();
 
-  var today = new Date();
-  let dd = today.getDate();
-  let mm = today.getMonth()+1; //January is 0 so need to add 1 to make it 1!
-  let yyyy = today.getFullYear();
-  let minutes = today.getMinutes();
-  let hour = today.getHours();
-  let sec = today.getSeconds();
-  if(dd<10){
-    dd='0'+dd
+  const [docs, loading] = useCollectionData(query);
+  console.log(docs);
+
+  let refId = null;
+
+  async function handleSubmit(e) {
+    e.preventDefault();
+    // Add a new document with a generated id.
+
+    await addDoc(collection(db, "productsTest"), {
+      prodType: prodType.current.value,
+      prodName: prodName.current.value,
+      brandCategory: brandCategory.current.value,
+      prodDescription: prodDescription.current.value,
+      productCode: new Date().getTime(),
+    }).then(async (docRefRes) => {
+      console.log("Document written with ID: ", docRefRes.id);
+      refId = docRefRes.id;
+    });
+
+    //   const docRef= doc(db, prodType.current.value,prodColor.current.value);
+    //   await setDoc(docRef, {prodType: prodType.current.value, prodName: prodName.current.value, brandCategory: brandCategory.current.value, prodDescription: prodDescription.current.value, })
+    //   // we use addDoc if we use the auto generated id in this case we are using name
+    //  //and if we use id we must not use the path prop
+    //  // await setDoc(docRef, {path})
+    //   e.target.reset()
+    //   console.log('added');
   }
-  if(mm<10){
-    mm='0'+mm
-  }
-  today = yyyy+'-'+mm+'-'+dd+'-'+ hour + ":" + minutes +':' + sec;
-    const [brand, setBrand] = useState("");
-    const [category, setCategory] = useState("");
-    const [productName, setProductName] = useState("");
-    const [image, setImage] = useState("");
-    const [Price, setPrice] = useState("");
-    const [description, setDescription] = useState("");
-    const [Size, setSize] = useState("");
-    const [Colors, setColors] = useState("");
-   
-   const [ productCode,  setProductCode] =useState("")
-    const usersCollectionRef = collection(db, "products");
-
-    const [allInfo, setAllInfo] = useState([]);
-
-
-
-
-    const [xS, setSizeXs] = useState("");
-    const [s, setSizeS] = useState("");
-    const [m, setSizeM] = useState("");
-    const [l, setSizeL] = useState("");
-    const [xL, setSizeXl] = useState("");
-    const [xXl, setSize2Xl] = useState("");
-    const [xXxl, setSize3Xl] = useState("");
-
-
-      const [ojbHandler, setObjHandler] = useState([]);
-      const arrObj = [];
-      const uidd = uid();
-    useEffect(() => {
-      
-          // read
-      const dbRef = ref_database(getDatabase());
-      get(child(dbRef, `${uidd}`)).then((snapshot) => {
-     if (snapshot.exists()) {
-      console.log(snapshot.val());
-      let keys = Object.keys(snapshot.val())
-      const Key = snapshot.key;
-      const Data = snapshot.val();
-
-      let arr = []
-      for (var x = 0; x < keys.length; x++){
-        arr.push(Data[keys[x]])
+  async function colorSubmit(e) {
+    e.preventDefault();
+    await setDoc(
+      doc(
+        db,
+        "productsTest",
+        refId,
+        "colours",
+        prodColor.current.value + "_" + prodSizes.current.value
+      ),
+      {
+        price: prodPrice.current.value,
+        qty: prodQty.current.value,
+        size: prodSizes.current.value,
+        colour: prodColor.current.value,
       }
-      console.log(arr)
-      setObjHandler(arr);
-     
-  } else {
-    console.log("No data available");
+    ).then((docRef) => {
+      console.log("added: ", docRef);
+    });
+    //   const docRef= doc(db, prodType.current.value,prodColor.current.value);
+    //   await setDoc(docRef, {prodType: prodType.current.value, prodName: prodName.current.value, brandCategory: brandCategory.current.value, prodDescription: prodDescription.current.value, })
+    //   // we use addDoc if we use the auto generated id in this case we are using name
+    //  //and if we use id we must not use the path prop
+    //  // await setDoc(docRef, {path})
+    //   e.target.reset()
   }
-}).catch((error) => {
-  console.error(error);
-});
-        
-        
-      },[]);
-
-      const add = async () => {
-        // const prodSizes = 'S';
-        await addDoc(usersCollectionRef, {
-          brandName: brand, category: category, description: description,price:Price,xS:xS,xL,Colors:Colors,
-          productCode: productCode, productName: productName, timeStamp: new Date()
-        }).then(async(r) => {
-           const prodColle = doc(db, "products", r.id, 'colours', 'black');
-           await setDoc(prodColle, { size: { price: "newPrice", qty: "newQty", size: "newSize" } }).then(() => {
-            console.log('Finished', r.id);
-          }).catch(er => {
-            console.log(er.message)
-          });;
-        }).catch(er => {
-            console.log(er.message)
-          });;
-    
-      };
-
 
   return (
-    
-    <CmsCenter/>,
-   
-    
-       <div className='rightSideProductsInfo'>
-        <form className='formProduct'>
-           <div className='Addprod'>
-              <div className='prod'>
-                 <p>Add Product</p>
-                    <select  onChange={(text) => {
-                       setBrand(text.target.value);
-                      }}>
-                       <option value="1">Select Brand</option>
-                          <option value="Brand 1">Brand 1</option>
-                          <option value="Brand 2">Brand 2</option>
-                          <option value="Brand 3">Brand 3</option>
-                          <option value="Brand 4">Brand 4</option>
-                    </select>
-                    <button>+</button>
-              </div>
-              <div >
-                    <select name="" id=""  onChange={(text) => {
-                        setCategory(text.target.value);
-                     }} >
-                      <option value="Category 1">Select Category</option>
-                         <option value="Category 2">Summer</option>
-                         <option value="Category 3">Winter</option>
-                         <option value="Category 4">Accessories</option>
-                         <option value="Category 5">Sale</option>
-                     </select>
-                     <button>+</button>
+    (<CmsCenter />),
+    (
+      <div className="rightSideProductsInfo">
+        {loading && "Loading..."}
+        <form className="formProduct" onSubmit={handleSubmit}>
+          <div className="Addprod">
+            <div className="prod">
+              <h2>Add Product</h2>
+              <select ref={prodType}>
+                <option value="1">Select Type</option>
+                <option value="T-shirt">T-shirt</option>
+                <option value="Shorts">Shorts</option>
+                <option value="Shirt">Shirt</option>
+                <option value="Denim">Denim</option>
+              </select>
             </div>
-            
-               <div>
-                   <input type="text" placeholder='Product Name' onChange={(text) => {
-                    setProductName(text.target.value);
-                   }} ></input>
-               </div>
-               <div>
-                <input type='number' step="0.01" placeholder='Price'
-                   onChange={(text) => {
-                    setPrice(text.target.value);
-                  }}
-                ></input>
-               </div>
-            
             <div>
-                <textarea type='text' placeholder='About the product'
-                onChange={(text) => {
-                    setDescription(text.target.value);
-                  }}
-                ></textarea>
+              <select name="" id="" ref={brandCategory}>
+                <option value="Category">Select Category</option>
+                <option value="Category 2">Summer</option>
+                <option value="Category 3">Winter</option>
+                <option value="Category 4">Accessories</option>
+                <option value="Category 5">Sale</option>
+              </select>
             </div>
-            </div> 
 
-            <div className='Addsizes'>
-               <div className='sizes'>
-                  <p>Available sizes</p>
-                    <input type="checkbox" value="XS" placeholder='Available size'
-                      onChange={(text) => {
-                       setSizeXs(text.target.value);
-                       }}
-                     ></input>
-                    <label>XS</label>
-                    <input type="checkbox" value="S" placeholder='Available size'
-                      onChange={(text) => {
-                        setSizeS(text.target.value);
-                        }}
-                     ></input>
-                    <label>       S       </label>
-                <input type="checkbox" value="M" placeholder='Available size'
-                onChange={(text) => {
-                    setSizeM(text.target.value);
-                  }}
-                ></input>
-                 <label>        M     </label>
-                <input type="checkbox" value="L" placeholder='Available size'
-                onChange={(text) => {
-                    setSizeL(text.target.value);
-                  }}
-                ></input>
-                 <label>    L</label>
-                <input type="checkbox" value="XL" placeholder='Available size'
-                onChange={(text) => {
-                    setSizeXl(text.target.value);
-                  }}
-                ></input>
-                <label>XL</label>
-                <input type="checkbox" value="2XL" placeholder='Available size'
-                onChange={(text) => {
-                    setSize2Xl(text.target.value);
-                  }}
-                ></input>
-                  <label>2XL</label>
-                <input type="checkbox" value="3XL" placeholder='Available size'
-                onChange={(text) => {
-                    setSize3Xl(text.target.value);
-                  }}
-                ></input>
-                <label>3XL</label>
+            <div>
+              <input type="text" placeholder="Product Name" ref={prodName} />
             </div>
-          
-            <div className='imagecon' >
-            <p><input type='file'
-              
-             
-             ></input>+
-              </p>
+
+            <div>
+              <textarea
+                type="text"
+                placeholder="About the product"
+                ref={prodDescription}
+              />
             </div>
-         
+            <div>{/* <input type='file' ref={prodImage}/> */}</div>
+            <div>
+              <button type="submit">ADD PRODUCT</button>
             </div>
-            {/* <select id=""  onChange={(text) => {
-                    setColors(text.target.value);
-                  }}>
-                
-                <option value="black">black</option>
-                <option value="red">red</option>
-                <option value="orange">orange</option>
-                <option value="yellow">yellow</option>
-                <option value="white">white</option>
-                <option value="1">Select Colors</option>
-                <input type="checkbox" value="M" placeholder='Available size'
-                onChange={(text) => {
-                    setSizeM(text.target.value);
-                  }}
-                ></input>
-            </select> */}
-                 <div className='Addcolor'>
-                  <div className='color'>
-                    <p>Available colors</p>
-                     <input type="checkbox" value="black" 
-                        onChange={(text) => {
-                          setColors(text.target.value);
-                          }}
-                       ></input>
-                      <label><div className='colorblock'></div></label>
-               
-                 
-                      <input type="checkbox" value="red" 
-                        onChange={(text) => {
-                          setColors(text.target.value);
-                         }}
-                      ></input>
-                      <label><div className='colorblock1'></div></label>
-               
-                      <input type="checkbox" value="orange" 
-                       onChange={(text) => {
-                        setColors(text.target.value);
-                        }}
-                      ></input>
-                     <label><div className='colorblock2'></div></label>
-                   <input type="checkbox" value="yellow" 
-                     onChange={(text) => {
-                      setColors(text.target.value);
-                       }}
-                    ></input>
-                 <label><div className='colorblock3'></div></label>
-                 
-                  <input type="checkbox" value="white" 
-                   onChange={(text) => {
-                    setSize(text.target.value);
-                  }}
-                   ></input>
-                  <label><div className='colorblock4'></div></label>
-              
-            
-           
-            </div>
-           
-            </div>
-            <div className='Buttonxontainer'>
-                 <input type="text" placeholder='Product Code' onChange={(text) => {
-                    setProductCode(text.target.value);
-                   }} >
-                 </input>
-                <p>or</p>
-                <button className='button1'>
-                  GENERATE CODE
-                </button >
-                <button
-            type="button"
-            className="btn btn-secondary btn-block"
-            onClick={add}
-            >Add</button>
-                <button className='button3'>
-                 CLEAR FORM
-                </button>
-            </div>
-            
+          </div>
+
+          <input type="alphanumeric" placeholder="Code"></input>
         </form>
-        </div>
-        
-        
-    
-  
-  )
+        <form className="formProduct" onSubmit={colorSubmit}>
+          <p>Available colors</p>
+          <select ref={prodColor}>
+            <option value="default">Select Type</option>
+            <option value="black">black</option>
+            <option value="red">red</option>
+            <option value="blue">blue</option>
+            <option value="white">white</option>
+
+            <option value="XL">XL</option>
+            <option value="XXL">XXL</option>
+            <option value="XXXL">XXXL</option>
+          </select>
+          <div className="sizes">
+            <p>Available sizes</p>
+            <select ref={prodSizes}>
+              <option value="XS">Select Type</option>
+              <option value="XS">XS</option>
+              <option value="S">S</option>
+              <option value="M">M</option>
+              <option value="L">L</option>
+
+              <option value="XL">XL</option>
+              <option value="XXL">XXL</option>
+              <option value="XXXL">XXXL</option>
+            </select>
+          </div>
+          <input
+            type="number"
+            step="0.01"
+            placeholder="Price"
+            ref={prodPrice}
+          ></input>
+          <input type="number" placeholder="Quantity" ref={prodQty}></input>
+          <button type="submit">Add</button>
+        </form>
+       
+      </div>
+    )
+  );
 }
 
-export default Add
+export default Add;
