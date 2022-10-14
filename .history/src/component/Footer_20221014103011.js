b51d96a9e0fcc5b1b18dab274119e8e050bf33ee
Bongani <bonganicele806@gmail.com>
2022-10-14 11:16:41 +0200

    editedAdd

---
+++ .history/src/component/Footer_20221014103011.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style="po" >
+      <h3>Thack</h3>
+    </div>
+  )
+}
+
+export default Footer
---
+++ .history/src/component/Footer_20221014103056.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <div style="position: absolute;
+    bottom: 0;
+    background-color: black;
+    width" >
+      <h3>Thack</h3>
+    </div>
+  )
+}
+
+export default Footer
